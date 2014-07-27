8cd68fb795295b3a26d30f48f5529389b5ebd4b0
Matthew Wigginton Conway <matt@indicatrix.org>
2014-07-26 20:02:24 -0500

    Fix bug with matrices with >= 10 columns

--- readcsv.py
+++ readcsv.py
@@ -11,31 +11,37 @@ NOTE: Manually edited csv file twice to match FIELD NAME format
 
 """
 import csv
-datareader = csv.DictReader(open("C:/Users/pgao/Documents/DATA_FIELD_DESCRIPTORS.csv"))
+
+datareader = csv.DictReader(open("sf1_data_field_descriptors_2010.csv"))
 data = []
-entry = {}
+entry = None
 current_table = ""
 for line in datareader:
     new_table_number = line['TABLE NUMBER']
     if new_table_number != current_table:
+        # save the old one
+        if entry != None:
+            data.append(entry)
+
         entry = {}
         current_table = new_table_number
         entry['Matrix Number'] = line['TABLE NUMBER']
         entry['File Name'] = line['SEGMENT']
         next_line = datareader.next()
         entry['Universe'] = (next_line['FIELD NAME'][9:].lstrip())
-        try:
-            entry['Name'] = line['FIELD NAME'][:line['FIELD NAME'].index('[')-1]
-            entry['Cell Count'] = line['FIELD NAME'][line['FIELD NAME'].index('[')+1]
-        except ValueError: 
-            print line
-        data.append(entry)
 
-#Write the tsv file
-datawriter = csv.DictWriter(open("C:/Users/pgao/Documents/SF1.tsv", "w"),
+        entry['Name'] = line['FIELD NAME'][:line['FIELD NAME'].index('[')-1]
+        entry['Cell Count'] = 0
+
+    # Increment the cell count iff there's actually data, rather than this being a descriptive row
+    if len(line['FIELD CODE']) > 0:
+        entry['Cell Count'] += 1
+
+# Write the tsv file
+datawriter = csv.DictWriter(open("sf1_2010.tsv", "w"),
                             ['File Name', 'Matrix Number',
                             'Cell Count', 'Name', 'Universe'],
-                            delimiter = '\t',
-                            lineterminator='\n')
+                            dialect = 'excel-tab'
+)
 datawriter.writeheader()
 datawriter.writerows(data)
